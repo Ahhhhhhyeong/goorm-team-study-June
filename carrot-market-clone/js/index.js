277864e909fb61e11e0ff218e64dd6f8d695c29c
Ahhhhhyeong <dkdud081@gmail.com>
2025-06-09 17:59:05 +0900

    [0609]1. css 분리(헤더, 푸터) / 2. 헤더 버튼 포지셔닝 끝 / 3. 메인 영역 진행중

---
+++ carrot-market-clone/js/index.js
@@ -0,0 +1,11 @@
+//요소 찾기
+const headlineEl = document.querySelector('#headlineText');
+
+const headlineArr = ['러닝모임', '알바', '중고거래', '부동산', '맛집'];
+let idx = 0;
+
+//헤드라인에 글자 사라졌다가 나타나는 효과
+setInterval(() => {
+  //headlineEl.classList.add('hidden');
+  //fade in fade out
+}, 2000);
